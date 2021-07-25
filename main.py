7dd614b682b341be977342299328c6f35cb7cc4f
muhammad erlangga <erlanggamuhammad01@gmail.com>
2021-07-25 18:57:24 +0700

    feat: finish rox bot

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
 import logging
 from telegram.ext import *
-import responses
-
+from responses import Responses
 API_KEY = '1900563339:AAHAYO6qrQy-EuOV46pVYoSY-cbAg6wWXqY'
 
 # Set up the logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logging.info('Starting Bot...')
 
-
 def start_command(update, context):
-    responses.reset_message()
-    update.message.reply_text('Please input your item amount')
+    response.reset_message()
+    update.message.reply_text('Hello there! I\'m a ROX Profit bot. What\'s up?')
+    update.message.reply_text('Please input your item amount and price like this:\namount-price')
+
+def result_command(update, context):
+    result = response.get_result()
+    if (isinstance(result, float)):
+        update.message.reply_text('your profit {}'.format(result))
+    else:
+        update.message.reply_text(result)
+
+
+def market_price_command(update, context):
+    update.message.reply_text("Please enter market price for the item like this:\nmp-price \nfor example mp-3000")
+
+def quantity_command(update, context):
+    update.message.reply_text("please enter quantity you want to sell like this:\nq-quantity \nfor example q-100")
+
+def tax_command(update, context):
+    update.message.reply_text("please enter your current text like this:\nt-tax\nfor example t-0.25")
 
 def end_command(update, context):
-    responses.reset_message()
+    response.reset_message()
     update.message.reply_text('Flow ended')
     update.message.reply_text('Please type /start to predict another profit')
 
 def help_command(update, context):
-    update.message.reply_text('You can control me by sending these commands: \n /start - predict new profit \n /end end the current flow')
+    update.message.reply_text('You can control me by sending these commands: \n/start - predict new profit \n/end - end the current flow\n/h - see commands\n/marketprice - input market price\n/quantity - input quantity \n/tax - input your current tax \n/result : to get your profit result')
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
     logging.info(f'User ({update.message.chat.id}) says: {text}')
 
     # Bot response
-    response = responses.get_response(text)
-    update.message.reply_text(response)
+    return_response = response.get_response(text)
+    update.message.reply_text(return_response)
 
 def error(update, context):
     # Logs errors
@@ -35,13 +51,20 @@ def error(update, context):
 
 # Run the programme
 if __name__ == '__main__':
+    response = Responses()
+
     updater = Updater(API_KEY, use_context=True)
     dp = updater.dispatcher
 
     # Commands
     dp.add_handler(CommandHandler('start', start_command))
-    dp.add_handler(CommandHandler('end', start_command))
+    dp.add_handler(CommandHandler('result', result_command))
+    dp.add_handler(CommandHandler('marketprice', market_price_command))
+    dp.add_handler(CommandHandler('quantity', quantity_command))
+    dp.add_handler(CommandHandler('tax', tax_command))
+    dp.add_handler(CommandHandler('end', end_command))
     dp.add_handler(CommandHandler('help', help_command))
+    dp.add_handler(CommandHandler('h', help_command))
 
     # Messages
     dp.add_handler(MessageHandler(Filters.text, handle_message))
--- responses.py
+++ responses.py
@@ -1,14 +1,70 @@
-import re
+class Responses():
+    list_amount=[]
+    list_price=[]
+    market_price=0.0
+    quantity=0.0
+    tax=0.0
 
-list_message=[]
+    return_message="type following command to fill required data:\n/marketprice : input market price\n/quantity : input quantity \n/tax : input your current tax \n\nor /result : to get your profit result"
+    add_more_message="do you want to add more item? (y/n)"
 
-def reset_message():
-    list_message.clear()
+    def reset_message(self):
+        self.list_amount.clear()
+        self.list_price.clear()
+        self.market_price = 0.0
+        self.quantity = 0.0
+        self.tax = 0.0
 
-def get_response(message):
-    list_message.append(message)
-    return list_message
+    def get_result(self):
+        if (len(self.list_amount)==0):
+            return "Please enter your item first\namount-price"
+        if (self.market_price==0.0):
+            return "Please enter market price for the item like this:\nmp-price \nfor example mp-3000"
+        if (self.quantity==0.0):
+            return "please enter quantity you want to sell like this:\nq-quantity \nfor example q-100"
+        if (self.tax==0.0):
+            return "please enter your current tax like this:\nt-tax\nfor example t-0.25"   
+        modal_satuan = 0.0
+        for i in range(len(self.list_price)):
+            modal_satuan += self.list_price[i] * self.list_amount[i]
+        modal_total = modal_satuan*self.quantity
+        revenue = self.market_price * self.quantity
+        tax_price = self.tax * revenue
+        result = revenue-tax_price-modal_total
+        return result
 
-# Test your system
-# get_response('What is your name bruv?')
-# get_response('Can you help me with something please?')
+    def get_response(self, message):
+        fw = message.split("-")[0]
+        if (not fw.isnumeric()):
+            fw = fw.lower()
+            if (fw=="n"):
+                return self.return_message
+            elif (fw=="y"):
+                return 'Please input your item amount and price like this:\namount-price'
+
+            try:
+                sw = float(message.split("-")[1])
+                if (fw=="mp"):
+                    self.market_price=sw
+                    return self.return_message
+                elif (fw=="q"):
+                    self.quantity=sw
+                    return self.return_message
+                elif (fw=="t"):
+                    self.tax=sw
+                    return self.return_message
+                else:
+                    return "Sorry, I dont understand that"
+            except:
+                return "Sorry, I dont understand that"
+
+        try:
+            sw = float(message.split("-")[1])
+            amount = float(fw)
+            price = sw
+            self.list_amount.append(amount)
+            self.list_price.append(price)
+
+            return self.add_more_message
+        except:
+            return "Sorry, I dont understand that"
